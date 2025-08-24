70d80b2e7b2b0f72b27c78950020d1445eb316d1
Dev Zenithh <husniposa@gmail.com>
2025-08-24 15:10:32 +0700

    change railway

--- app.py
+++ app.py
@@ -59,4 +59,6 @@ def stats():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    serve(app, host="0.0.0.0", port=port)
